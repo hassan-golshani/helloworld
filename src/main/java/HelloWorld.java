f65387886a87a1b58a1027f2796d98a4fb06fd7e
golshani <golshani@modernisc.com>
2016-05-18 17:13:04 +0430

    add Obj class

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,9 @@
+import pack.Obj;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Obj obj = new Obj(Long.valueOf(1), "HassanGolshani");
+        System.out.println(obj);
     }
 }
---
+++ src/main/java/pack/Obj.java
@@ -0,0 +1,35 @@
+package pack;
+
+public class Obj {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Obj(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Obj{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
